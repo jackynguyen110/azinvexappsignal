eaf7fbcaac99f24d1ad80318351f3b54a2b06d7d
longdhit <longdhit@gmail.com>
2018-09-17 16:35:49 +0700

    up

--- src/app/common/form/TextInputForm.js
+++ src/app/common/form/TextInputForm.js
@@ -2,18 +2,12 @@ import React from 'react'
 
 const TextInputForm = ({input, props, width, type, placeholder, title, meta: {touched, error}}) => {
   return (
-<<<<<<< HEAD
     <div class={'form-group ' || (touched && !!error && 'issue')}>
       {title &&  <h5>{title}</h5>}
       <div class="control">
         <input className="form-control" {...input} {...props} placeholder={placeholder} type={type}/>
         {touched && error && <div class="help-block"><ul role="alert"><li>{error}</li></ul></div>}
       </div>
-=======
-    <div className={'form-group ' || (touched && !!error && 'issue')}>
-      <input className="form-control" {...input} {...props} placeholder={placeholder} type={type}/>
-      {touched && error && <div className="help-block"><ul role="alert"><li>{error}</li></ul></div>}
->>>>>>> 3d0bf229854ef2f9e1a6dac361d7ad7777b5821c
     </div>
   )
 }
