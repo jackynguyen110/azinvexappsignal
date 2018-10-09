8f43994c53a1e9d40dfb47161cf795c5909ffc0e
unknown <longdhit@gmail.com>
2018-10-09 14:53:25 +0700

    up

--- src/Components/Auth/authActions.js
+++ src/Components/Auth/authActions.js
@@ -36,6 +36,7 @@ export const registerUser = (user) =>
         displayName: user.name,
         role: "member",
         email: user.email,
+        photoURL: null,
         createdAt: firestore.FieldValue.serverTimestamp()
       }
       await firestore.set(`users/${createdUser.uid}`, {...newUser})
