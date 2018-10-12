ded66d0cadc62791f0c487a609959bb644859a02
longdhit <longdhit@gmail.com>
2018-10-12 10:37:20 +0700

    up

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -15,14 +15,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 
-messaging.requestPermission().then(function () {
-  console.log('Notification permission granted.');
-  // TODO(developer): Retrieve an Instance ID token for use with FCM.
-  // ...
-}).catch(function (err) {
-  console.log('Unable to get permission to notify.', err);
-});
-
 const settings = {
   timestampsInSnapshots: true
 }
